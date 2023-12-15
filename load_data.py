093df4a34acfe7100324dd9e3c19bbd6b02e6f74
Jackson Walters <jacksonwalters@gmail.com>
2023-12-15 15:03:29 -0500

    print status during loads

--- load_data.py
+++ load_data.py
@@ -16,10 +16,12 @@ def datestr_to_int(datetime_str,date_format):
 #function to load all data
 def merged_df():
 
+    print("loading train data...")
     #load the training data, dropping NaN's
     train = pd.read_csv("train.csv").dropna()
     train['datetime'] = train['datetime'].apply(lambda x: datestr_to_int(x,'%Y-%m-%d %H:%M:%S'))
 
+    print("loading gas_prices...")
     #load gas_prices
     gas_prices = pd.read_csv("gas_prices.csv")
     #convert date strings to ints
@@ -27,6 +29,7 @@ def merged_df():
     gas_prices = gas_prices.drop(columns=['origin_date'])
     gas_prices['data_block_id'] -= 1
 
+    print("loading electricity_prices...")
     #load electricity_prices
     electricity_prices = pd.read_csv("electricity_prices.csv")
     #convert date strings to ints
@@ -34,6 +37,7 @@ def merged_df():
     electricity_prices = electricity_prices.drop(columns=['origin_date'])
     electricity_prices['data_block_id'] -= 1
 
+    print("loading forecast_weather...")
     #load forecast_weather
     forecast_weather = pd.read_csv("forecast_weather.csv")
     #convert strings to ints
@@ -42,14 +46,17 @@ def merged_df():
     forecast_weather = forecast_weather.rename(columns={'forecast_datetime':'forecast_date'})
     forecast_weather['data_block_id'] -= 1
 
+    print("merging train and gas_prices...")
     #merge gas prices and train.csv data
     #column names differ, so use left_on and right_on
     df = pd.merge(train, gas_prices, left_on=['data_block_id','datetime'], right_on=['data_block_id','forecast_date'], how='left')
 
+    print("merging electricity_prices...")
     #merge train and gas_prices via left join on data_block_id
     #this leaves all rows of train, but matches
     df = df.merge(electricity_prices, on=['data_block_id','forecast_date'], how='left')
 
+    print("merging forecast_weather...")
     #merge forecast_weather on forecast date
     df = df.merge(forecast_weather, on=['data_block_id','forecast_date'],how='left')
 
